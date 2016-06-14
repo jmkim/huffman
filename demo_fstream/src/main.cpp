fef763451707d56a487991a80632bff2855fd3f8
Jongmin Kim <jmkim@pukyong.ac.kr>
2016-06-14 12:22:35 +0900

    Add a binary printer into demo_fstream

--- demo_fstream/src/main.cpp
+++ demo_fstream/src/main.cpp
@@ -2,6 +2,38 @@
 #include <sstream>
 #include <string>
 
+template<typename T = char>
+void
+print_binary(T src, const size_t & byte_size)
+{
+    const char  zerobit     = 0;
+    const char  nonzerobit  = 1;
+          int   bpos;
+
+    while(src != 0)
+    {
+        char buffer[byte_size] = { 0, };
+
+        for(bpos = byte_size - 1; bpos >= 0; --bpos)
+        {
+            buffer[bpos] = ((src % 2 == 0) ? zerobit : nonzerobit);
+            src = src >> 1;
+        }
+
+        if(bpos < 0)
+            bpos = 0;
+
+        for(int i = 0; i < byte_size; ++i)
+        {
+            if(i != 0 && i % 4 == 0)
+                printf(" ");
+            printf("%d", buffer[i]);
+        }
+
+        std::cout << "(" << bpos << ")" << std::endl;
+    }
+}
+
 int
 main(void)
 {
@@ -38,11 +70,11 @@ main(void)
             buffer[buf_counter++] = raw;
 
             {
-                printf("[INP] %2x\n", raw);
+                printf("[INP] %02x\n", raw);
                 printf("[BUF] ");
 
                 for(int i = 0; i < buf_counter; ++i)
-                    printf("%2x ", buffer[i]);
+                    printf("%02x ", buffer[i]);
 
                 printf("\n");
             }
@@ -51,15 +83,22 @@ main(void)
             fout.seekp(0, fin.end);
             fout.write(&raw, sizeof(int8_t));
         }
+
     }
 
     {
         printf("[FIN] ");
 
         for(auto ch : fout.str())
-            printf("%2x ", ch);
+            printf("%02x ", ch);
 
         printf("\n");
+
+        for(auto ch : fout.str())
+        {
+            printf("      %02x: ", ch);
+            print_binary<char> (ch, 8);
+        }
     }
 
     {
