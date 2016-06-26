7eac3228f4d31dc0926bb0366b5fc86e54398fc5
Jongmin Kim <jmkim@pukyong.ac.kr>
2016-06-27 00:49:39 +0900

    Rewrite demo

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -2,57 +2,197 @@
 
 #include <iostream>
 #include <fstream>
+#include <cstring>
+
+#include <getopt.h>
 
 using namespace algorithm;
 
-static enum
+struct Msg
 {
-    E_SUCCESS = 0,
-    E_ARGUMENT_NOT_PROVIDED,
-    E_FILE_NOT_FOUND,
-}
-StatusType;
+    static void
+    Version(std::ostream & out)
+    { out << "Huffman encoding with RLE compressor\n"; }
+
+    static void
+    Usage(std::ostream & out, const std::string & this_file)
+    { out << "Usage: " << this_file << " [OPTION]... [INPUT FILENAME]\n"; }
+
+    static void
+    Help(std::ostream & out, const std::string & this_file)
+    {
+        Version(out);
+        Usage(out, this_file);
+        out << "\nMandatory arguments to long options are mandatory for short options too.\n"
+            << "\n"
+            << "Options:\n"
+            << "  -h,  --help                      print this help\n"
+            << "  -c,  --compress                  compress with huffman coding (this is the default)\n"
+            << "  -d,  --decompress                decompress with huffman decoding\n"
+            << "  -o,  --output-file=FILENAME      specify the output path (default if stdout)\n";
+    }
+
+    static void
+    InvalidOption(std::ostream & out, const std::string & this_file)
+    {
+        Usage(out, this_file);
+        out << "\nTry `" << this_file << " --help' for more options.\n";
+    }
+
+    static void
+    ArgumentNotProvided(std::ostream & out, const std::string & this_file)
+    {
+        out << this_file << ": missing argument\n";
+        InvalidOption(out, this_file);
+    }
+
+    static void
+    TooManyArguments(std::ostream & out, const std::string & this_file)
+    {
+        out << this_file << ": too many arguments\n";
+        InvalidOption(out, this_file);
+    }
+
+    static void
+    CannotOpenFile(std::ostream & out, const std::string & fin_path)
+    { out << fin_path << ": " << strerror(ENOENT) << "\n"; }
+    
+    static void
+    CompressFailed(std::ostream & out, const std::string & path)
+    { out << "Cannot compress file " << path << ".\n"; }
+    
+    static void
+    DecompressFailed(std::ostream & out, const std::string & path)
+    { out << "Cannot decompress file " << path << ".\n"; }
+};
 
 int
-main(const int argc, const char * argv[])
+main(const int argc, char * const argv[])
 {
-    if(argc < 2)
-    {
-        std::cerr << "Usage: " << argv[0] << "[file name]\n";
-        return E_ARGUMENT_NOT_PROVIDED;
-    }
+    int retval = 0;
 
-    const   std::string fin_cmp_path    = argv[1],
-                        fout_cmp_path   = fin_cmp_path + ".z",
-                        fout_dec_path   = fout_cmp_path + ".dec";
+    std::string fin_path;
+    std::string fout_path;
+    bool compress = true;
 
     {
-        std::ifstream fin(fin_cmp_path, std::ios::binary);
-        if(! fin.is_open())
+        /** getopt(3) */
+
+        int c;
+        int digit_optind = 0;
+
+        while(true)
         {
-            std::cerr << "Error: File '" << fin_cmp_path << "' not found.\n";
-            return E_FILE_NOT_FOUND;
+            int this_option_optind = optind ? optind : 1;
+            int option_index = 0;
+            static struct option options[] =
+            {
+                { "compress",       no_argument,        nullptr, 'c' },
+                { "decompress",     no_argument,        nullptr, 'd' },
+                { "help",           no_argument,        nullptr, 'h' },
+                { "output-file",    required_argument,  nullptr, 'o' }
+            };
+
+            c = getopt_long(argc, argv, "cdho:", options, &option_index);
+
+            if(c == -1)
+                break;
+
+            switch(c)
+            {
+            case 'c': /** --compress */
+                compress = true;
+                break;
+
+            case 'd': /** --decompress */
+                compress = false;
+                break;
+
+            case 'o': /** --output-file */
+                fout_path = optarg;
+                break;
+
+            case 'h': /** --help */
+                Msg::Help(std::cout, argv[0]);
+                goto jump_exit;
+
+            case '?': /** Invalid option */
+                Msg::InvalidOption(std::cout, argv[0]);
+                goto jump_exit;
+
+            default:
+                break;
+            }
         }
 
-        std::ofstream fout(fout_cmp_path, std::ios::binary);
+        if(optind + 1 == argc) /** Only one argument except options */
+            fin_path = argv[optind];
+        else
+        {
+            if(argc > 2)
+                Msg::TooManyArguments(std::cout, argv[0]);
+            else
+                Msg::ArgumentNotProvided(std::cout, argv[0]);
 
-        Huffman huffman;
-        huffman.CompressFile(fin, fout);
-
-        fin.close();
-        fout.close();
+            goto jump_exit;
+        }
     }
 
+    if(compress)
     {
-        std::ifstream fin(fout_cmp_path, std::ios::binary);
-        std::ofstream fout(fout_dec_path, std::ios::binary);
+        /** Compression */
+
+        std::ifstream fin(fin_path, std::ios::binary);
+        if(! fin.is_open())
+        {
+            Msg::CannotOpenFile(std::cerr, fin_path);
+            Msg::CompressFailed(std::cerr, fin_path);
+
+            retval = ENOENT;
+            goto jump_exit;
+        }
 
         Huffman huffman;
-        huffman.DecompressFile(fin, fout);
+
+        if(fout_path.empty())
+            huffman.Compress(fin, std::cout);
+        else
+        {
+            std::ofstream fout(fout_path, std::ios::binary);
+            huffman.Compress(fin, fout);
+            fout.close();
+        }
 
         fin.close();
-        fout.close();
+    }
+    else
+    {
+        /** Decompression */
+
+        std::ifstream fin(fin_path, std::ios::binary);
+        if(! fin.is_open())
+        {
+            Msg::CannotOpenFile(std::cerr, fin_path);
+            Msg::DecompressFailed(std::cerr, fin_path);
+
+            retval = ENOENT;
+            goto jump_exit;
+        }
+
+        Huffman huffman;
+
+        if(fout_path.empty())
+            huffman.Decompress(fin, std::cout);
+        else
+        {
+            std::ofstream fout(fout_path, std::ios::binary);
+            huffman.Decompress(fin, fout);
+            fout.close();
+        }
+
+        fin.close();
     }
 
-    return E_SUCCESS;
+jump_exit:
+    return retval;
 }
